d3980826b7b86dfa94fc5ef3898d08d97210e422
ruslan <bumerboy86@gmail.com>
2023-07-11 16:20:01 +0600

    изменены ссылки на сами тесты

--- src/components/TestsChose.vue
+++ src/components/TestsChose.vue
@@ -2,24 +2,23 @@
   <div class="chose">
     <h2>Выберите тест</h2>
     <p>Обновлено 29 июня 2023 г. Добавлен интерактивный поиск вопросов, темы тестирования открываются по клику</p>
-    <p>JavaScript: легкий-35, средний-39, сложный-67 вопрос/ответов</p>
     <div class="box">
       <router-link to="/javascript-light"  class="javascriptLink">
-        <img src="../assets/book-bookmark.svg" alt="MiddleTest" class="item-image"/>
-        <p class="text">Начальный</p>
-        <p class="text">JS</p>
+        <img src="../assets/answer.svg" alt="lightTest" class="item-image"/>
+        <p class="chose__text">Начальный</p>
+        <p class="chose__text_secondary">JavaScript: легкий-35, вопрос-ответ</p>
       </router-link>
 
       <router-link to="/javascript-middle"  class="javascriptLink">
-        <img src="../assets/book-bookmark.svg" alt="MiddleTest" class="item-image"/>
-        <p class="text">Средний</p>
-        <p class="text">JS</p>
+        <img src="../assets/answer.svg" alt="MiddleTest" class="item-image"/>
+        <p class="chose__text">Средний</p>
+        <p class="chose__text_secondary">JavaScript: средний-39, вопрос-ответ</p>
       </router-link>
 
       <router-link to="/javascript-strong"  class="javascriptLink">
-        <img src="../assets/book-bookmark.svg" alt="MiddleTest" class="item-image"/>
-        <p class="text">Сложный</p>
-        <p class="text">JS</p>
+        <img src="../assets/answer.svg" alt="MiddleTest" class="item-image"/>
+        <p class="chose__text">Сложный</p>
+        <p class="chose__text_secondary">JavaScript: сложный-67, вопрос-ответ</p>
       </router-link>
 
     </div>
@@ -42,6 +41,7 @@ export default {
   gap: 20px;
   width: 100%;
   flex-wrap: wrap;
+  justify-content: space-between;
 }
 
 .javascriptLink {
@@ -50,16 +50,29 @@ export default {
   justify-content: center;
   box-sizing: border-box;
   text-decoration: none;
-  color: #2c3e50;
+  color: #22253B;
   font-weight: bold;
+  width: 270px;
+  height: 254px;
+  background: #EAF2F5;
+  border-radius: 20px;
+  margin:  0 auto;
+  padding: 35px;
+  align-items: center;
 }
 
 .item-image {
   width: 120px;
+  margin-bottom: 18px;
 }
 
-.text {
+.chose__text {
   margin: 0 0 5px 0;
+}
+
+.chose__text_secondary {
+  font-weight: lighter;
+  margin: 0
 }
 
 </style>
